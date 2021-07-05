1331b8301c104ddd857160fd71e95ca32eb5c922
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-05 12:50:43 +0300

    #1 commmit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Deploymment pe zona</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
