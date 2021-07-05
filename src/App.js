a55b479870decfd6cbcfd1135034803bccf698b5
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-05 12:55:45 +0300

    modificare titlu

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploymment pe zona</p>
+          Mare inginerie app</p>
         
       </header>
     </div>
