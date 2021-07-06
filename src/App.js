dc3617b7542025dc2c06cffd1cde85617ad5944c
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-06 04:36:27 +0300

    schimbare test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mare inginerie app</p>
+          Mare inginere app</p>
         
       </header>
     </div>
