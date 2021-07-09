000a1d5df8fbf74a36fee5355c3e52fd7b50bfcf
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-10 01:12:20 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mare inginere app</p>
-        
-      </header>
-    </div>
+<h2>ika</h2>
   );
 }
 
