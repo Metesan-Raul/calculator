d55c7004b6fb24829bb2091643ed12b0de87c508
RAUL PETRU METESAN <raul.metesan@s.unibuc.ro>
2021-07-11 01:47:26 +0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Typed from "react-typed"
+const Header = () => {
+    return (
+        
+            <div className="header-wraper">
+                <div className="main-info">
+                    <h1>haide să îl studiem pe</h1>
+                    <Typed className="typed-text"
+                    strings={["Gauss","Pitagora","L'Hospital"]}
+                    typeSpeed={100}
+                    backSpeed={100}
+                    loop
+                />
+                <a href="#" className="btn-main-offer"></a>
+                </div>
+            </div>
+        
+    )
+}
+
+export default Header
